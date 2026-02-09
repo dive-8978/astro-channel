868b40a28c6788618bee68d5cfe5515714b90d6d
dive-8978 <wangbaohz@hotmail.com>
2026-02-09 17:04:55 +0800

    Update MemeAstro.sol

--- contracts/MemeAstro.sol
+++ contracts/MemeAstro.sol
@@ -2,78 +2,31 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/Ownable2Step.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MemeAstro is ERC20, Pausable, Ownable2Step {
-    using ECDSA for bytes32;
+contract MemeAstro is ERC20, Ownable {
+    uint256 public constant TOTAL_SUPPLY = 1_000_000_000_000 * 10 ** 18; // 1000亿 MA
+    mapping(address => uint256) public lockUntil;
 
-    string public constant NAME = "MemeAstro";
-    string public constant SYMBOL = "MA";
-    uint256 public constant DECIMALS = 18;
-    uint256 public constant TOTAL_SUPPLY = 100_000_000_000 * 10**DECIMALS; // 1000亿
-    uint256 public constant AIRDROP_SUPPLY = 35_000_000_000 * 10**DECIMALS; // 350亿
-    uint256 public constant LOCK_PERIOD = 90 days;
-
-    address public signer;
-    mapping(address => bool) public claimed;
-    mapping(address => uint256) public unlockTime;
-    uint256 public airdropClaimed;
-
-    event AirdropClaimed(address indexed user, uint256 amount, uint256 unlockTime);
-
-    constructor(address _signer) ERC20(NAME, SYMBOL) {
-        signer = _signer;
-        _mint(address(this), TOTAL_SUPPLY);
+    constructor() ERC20("MemeAstro", "MA") {
+        _mint(address(this), 350_000_000_000 * 10 ** 18); // 空投池 35%
+        _mint(msg.sender, 650_000_000_000 * 10 ** 18); // 生态发展 65%
     }
 
-    function claimAirdrop(uint256 rewardAmount, bytes calldata signature) external whenNotPaused {
-        address user = msg.sender;
-        require(!claimed[user], "Already claimed");
-        require(rewardAmount > 0, "Zero reward");
-        require(airdropClaimed + rewardAmount <= AIRDROP_SUPPLY, "Airdrop exhausted");
-
-        bytes32 message = keccak256(
-            abi.encodePacked(
-                "Claim MemeAstro Airdrop",
-                user,
-                rewardAmount,
-                block.chainid
-            )
-        );
-        require(
-            message.toEthSignedMessageHash().recover(signature) == signer,
-            "Invalid signature"
-        );
-
-        claimed[user] = true;
-        unlockTime[user] = block.timestamp + LOCK_PERIOD;
-        airdropClaimed += rewardAmount;
-        _transfer(address(this), user, rewardAmount);
-
-        emit AirdropClaimed(user, rewardAmount, unlockTime[user]);
+    function claim(address to, uint256 amount, uint256 unlockTime) external onlyOwner {
+        require(balanceOf(address(this)) >= amount, "Insufficient balance in contract");
+        _transfer(address(this), to, amount);
+        lockUntil[to] = unlockTime;
     }
 
-    function setSigner(address _newSigner) external onlyOwner {
-        signer = _newSigner;
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
-    }
-
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        if (block.timestamp < unlockTime[msg.sender]) revert("MA locked for 3 months");
-        return super.transfer(to, amount);
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
-        if (block.timestamp < unlockTime[from]) revert("MA locked for 3 months");
-        return super.transferFrom(from, to, amount);
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        if (from != address(0)) { // not mint
+            require(block.timestamp >= lockUntil[from], "Tokens are locked");
+        }
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
